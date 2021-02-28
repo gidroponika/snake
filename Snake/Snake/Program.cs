8a15d710ce76618832cce598ec03422ce09d88d4
Max <axa.fenixxx@meta.ua>
2021-02-28 18:55:15 +0200

    Function programming

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -2,8 +2,13 @@
 
 namespace Snake {
     class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+        static void Main() {
+            int x1 = 2;
+            int y1 = 4;
+            char sym1 = 'x';
+
+            Console.SetCursorPosition(x1, y1);
+            Console.WriteLine(sym1);
         }
     }
 }
