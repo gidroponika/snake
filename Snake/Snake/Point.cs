fdc55c2f3c9b3fd32e05e72dcf5687785c6aa175
Max <axa.fenixxx@meta.ua>
2021-02-28 19:09:05 +0200

    Create struct Point

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake {
+    struct Point {
+        public readonly int x;
+        public readonly int y;
+        char sym;
+        public Point(int x,int y,char sym) {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+        public void DrawSymbol() {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -3,12 +3,9 @@
 namespace Snake {
     class Program {
         static void Main() {
-            int x1 = 2;
-            int y1 = 4;
-            char sym1 = 'x';
-
-            Console.SetCursorPosition(x1, y1);
-            Console.WriteLine(sym1);
+            Point point = new Point(4, 3, 'x');
+            point.DrawSymbol();
+            Console.WriteLine();
         }
     }
 }
